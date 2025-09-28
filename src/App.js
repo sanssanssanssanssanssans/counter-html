839e03c4dee33a789d1353997fa1ed6b239a326a
sanssanssanssanssanssans <ajona5427@kakao.com>
2025-09-28 17:45:07 +0900

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+const Counter = () => {
+    const [count, setCount] = useState(0);
+    return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Counter: {count}</h1>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count - 1)}>Decrease</button>
+      <button onClick={() => setCount(0)}>Reset</button>
+    </div>
+  );
+}
+
+export default Counter;
